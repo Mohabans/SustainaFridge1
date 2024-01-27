09e6d425591b3b3bd80a6016c01de91740eb0a2b
Erik <erikwblake@gmail.com>
2024-01-27 15:41:04 -0500

    Created item class and added vars for each attribute.

--- app/src/main/java/com/example/sustainafridge/classes/item.kt
+++ app/src/main/java/com/example/sustainafridge/classes/item.kt
@@ -1,4 +1,15 @@
 package com.example.sustainafridge.classes
 
 class item {
+    enum class remaining {
+        FULL,
+        SEVENTYFIVE,
+        FIFTY,
+        TWENTYFIVE,
+        EMPTY
+    }
+    private var name : String = ""
+    private var portion : remaining = remaining.FULL
+    private var owner : String = ""
+
 }
